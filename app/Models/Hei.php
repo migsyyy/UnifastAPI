87ec4d041fbaa508f68ce7194def0c9b9552b3b0
migsyyy <m.gelorenzo@gmail.com>
2022-07-17 00:54:03 +0800

    modified a ot of stuff including the queries to include numbers and also added unicode handling when json encoding

--- app/Models/Hei.php
+++ app/Models/Hei.php
@@ -12,107 +12,40 @@ class Hei extends Model
     {
         $heitype = DB::table('tbl_heis')->select('hei_it')->groupBy('hei_it')->get();
 
-        echo json_encode($heitype);
+        echo json_encode($heitype,JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES);
     }
     public function getHeiRegions()
     {
-        $heiregion = DB::table('tbl_heis')->select('hei_region_nir')->groupBy('hei_region_nir')->get();
+        $heiregion = DB::table('tbl_heis')->select('hei_psg_region','hei_region_nir')->groupBy('hei_region_nir','hei_psg_region')->get();
 
-        echo json_encode($heiregion);
+        echo json_encode($heiregion,JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES);
     }
     public function getHeiProvinces($heiregion)
     {
-        if ($heiregion == '') {
-            $hei_region_nir = '';
-        }
-        if ($heiregion == 1) {
-            $hei_region_nir = '01 - Ilocos Region';
-        }
-        if ($heiregion == 2) {
-            $hei_region_nir = '02 - Cagayan Valley';
-        }
-        if ($heiregion == 3) {
-            $hei_region_nir = '03 - Central Luzon';
-        }
-        if ($heiregion == 4) {
-            $hei_region_nir = '04 - CALABARZON';
-        }
-        if ($heiregion == 5) {
-            $hei_region_nir = '05 - Bicol Region';
-        }
-        if ($heiregion == 6) {
-            $hei_region_nir = '06 - Western Visayas';
-        }
-        if ($heiregion == 7) {
-            $hei_region_nir = '07 - Central Visayas';
-        }
-        if ($heiregion == 8) {
-            $hei_region_nir = '08 - Eastern Visayas';
-        }
-        if ($heiregion == 9) {
-            $hei_region_nir = '09 - Zamboanga Peninsula';
-        }
-        if ($heiregion == 10) {
-            $hei_region_nir = '10 - Northern Mindanao';
-        }
-        if ($heiregion == 11) {
-            $hei_region_nir = '11 - Davao Region';
-        }
-        if ($heiregion == 12) {
-            $hei_region_nir = '12 - Soccsksargen';
-        }
-        if ($heiregion == 13) {
-            $hei_region_nir = '13 - National Capital Region';
-        }
-        if ($heiregion == 14) {
-            $hei_region_nir = '13 - NCR';
-        }
-        if ($heiregion == 15) {
-            $hei_region_nir = '14 - CAR';
-        }
-        if ($heiregion == 16) {
-            $hei_region_nir = '14 - Cordillera Administrative Region';
-        }
-        if ($heiregion == 17) {
-            $hei_region_nir = '15 - BARMM';
-        }
-        if ($heiregion == 18) {
-            $hei_region_nir = '15A - BARMM';
-        }
-        if ($heiregion == 19) {
-            $hei_region_nir = '15B - BARMM';
-        }
-        if ($heiregion == 20) {
-            $hei_region_nir = '16 - Caraga';
-        }
-        if ($heiregion == 21) {
-            $hei_region_nir = '17 - MIMAROPA';
-        }
+        $heiprovince = DB::table('tbl_heis')->select('hei_psg_region','hei_prov_name','hei_prov_code')->where('hei_psg_region', $heiregion)->groupBy('hei_prov_name','hei_psg_region','hei_prov_code')->get();
 
-        $heiprovince = DB::table('tbl_heis')->select('hei_prov_name')->groupBy('hei_prov_name')->where('hei_region_nir', $hei_region_nir)->get();
-
-        echo json_encode($heiprovince);
+        echo json_encode($heiprovince,JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES);
     }
 
     public function searchHeiName($heiname)
     {
         $hei = DB::table('tbl_heis')->select('hei_region_nir')->addSelect('hei_shortname')->addSelect('hei_it')->addSelect('hei_ct')->where('hei_shortname', 'like', '%' . $heiname . '%')->get();
 
-        echo json_encode($hei);
+        echo json_encode($hei,JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES);
     }
 
     public function getHeiInfo($heiuii)
     {
         $hei = DB::table('tbl_heis')->select('hei_region_nir')->addSelect('hei_shortname')->addSelect('hei_it')->addSelect('hei_ct')->addSelect('hei_prov_name')->addSelect('hei_email')->addSelect('hei_telno')->addSelect('hei_focal')->addSelect('hei_focal_contact')->addSelect('hei_focal_email')->where('hei_uii', '=', $heiuii)->get();
 
-        echo json_encode($hei);
+        echo json_encode($hei,JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES);
     }
 
     public function getCourses($heiuii)
     {
         $hei = DB::table('tbl_registry')->select('course_name')->addSelect('course_code')->where('hei_uii', '=', $heiuii)->get();
 
-        echo json_encode($hei);
+        echo json_encode($hei,JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES);
     }
 
     public static function showHei($heiregion, $heiprov, $heitype)
@@ -140,76 +73,10 @@ class Hei extends Model
         }
 
 
-        if ($heiregion == '') {
-            $hei_region_nir = '';
-        }
-        if ($heiregion == 1) {
-            $hei_region_nir = '01 - Ilocos Region';
-        }
-        if ($heiregion == 2) {
-            $hei_region_nir = '02 - Cagayan Valley';
-        }
-        if ($heiregion == 3) {
-            $hei_region_nir = '03 - Central Luzon';
-        }
-        if ($heiregion == 4) {
-            $hei_region_nir = '04 - CALABARZON';
-        }
-        if ($heiregion == 5) {
-            $hei_region_nir = '05 - Bicol Region';
-        }
-        if ($heiregion == 6) {
-            $hei_region_nir = '06 - Western Visayas';
-        }
-        if ($heiregion == 7) {
-            $hei_region_nir = '07 - Central Visayas';
-        }
-        if ($heiregion == 8) {
-            $hei_region_nir = '08 - Eastern Visayas';
-        }
-        if ($heiregion == 9) {
-            $hei_region_nir = '09 - Zamboanga Peninsula';
-        }
-        if ($heiregion == 10) {
-            $hei_region_nir = '10 - Northern Mindanao';
-        }
-        if ($heiregion == 11) {
-            $hei_region_nir = '11 - Davao Region';
-        }
-        if ($heiregion == 12) {
-            $hei_region_nir = '12 - Soccsksargen';
-        }
-        if ($heiregion == 13) {
-            $hei_region_nir = '13 - National Capital Region';
-        }
-        if ($heiregion == 14) {
-            $hei_region_nir = '13 - NCR';
-        }
-        if ($heiregion == 15) {
-            $hei_region_nir = '14 - CAR';
-        }
-        if ($heiregion == 16) {
-            $hei_region_nir = '14 - Cordillera Administrative Region';
-        }
-        if ($heiregion == 17) {
-            $hei_region_nir = '15 - BARMM';
-        }
-        if ($heiregion == 18) {
-            $hei_region_nir = '15A - BARMM';
-        }
-        if ($heiregion == 19) {
-            $hei_region_nir = '15B - BARMM';
-        }
-        if ($heiregion == 20) {
-            $hei_region_nir = '16 - Caraga';
-        }
-        if ($heiregion == 21) {
-            $hei_region_nir = '17 - MIMAROPA';
-        }
 
 
-        $hei = DB::table('tbl_heis')->select('hei_region_nir')->addSelect('hei_prov_name')->addSelect('hei_shortname')->addSelect('hei_it')->addSelect('hei_ct')->where('hei_it', 'like', '%' . $hei_it . '%')->where('hei_region_nir', '=', $hei_region_nir)->where('hei_prov_name', '=', $heiprov)->get();
+        $hei = DB::table('tbl_heis')->select('hei_region_nir')->addSelect('hei_prov_name')->addSelect('hei_shortname')->addSelect('hei_it')->addSelect('hei_ct')->where('hei_it', 'like', '%' . $hei_it . '%')->where('hei_psg_region', '=', $heiregion)->where('hei_prov_code', '=', $heiprov)->get();
 
-        echo json_encode($hei);
+        echo json_encode($hei,JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES);
     }
 }
